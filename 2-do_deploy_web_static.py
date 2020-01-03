05bb2560353f1dfe99dae88acaba4886d9ec57da
Michael <michael.sosa@holbertonschool.com>
2020-01-03 18:00:28 -0500

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,30 +11,30 @@ env.hosts = ['34.74.238.86', '35.196.226.115']
 
 
 def do_deploy(archive_path):
-    """ Script that does a bunch of things, I havent breakfast yet :( """
+    """ Script that does a lot of magic =) like penn and teller """
     if (archive_path is False or archive_path is None):
         return False
     try:
         # this will be the web_static_NUMBERSSSS.tgz
-        fileName = archive_path.split("/")[-1]
+        last = archive_path.split("/")[-1]
         # and this will be the stuff without the dot extension
-        completePath = "/data/web_static/releases/" + fileName.split(".")[0]
+        foldName = "/data/web_static/releases/" + last.split(".")[0]
         # overwrites pre-existing remote files without request confirmation
         put(archive_path, "/tmp/")
         # make the directory on the server
-        run("sudo mkdir -p {}".format(completePath))
+        run("sudo mkdir -p {}".format(foldName))
         # unzips the archive to the folder on the webserver
-        run("sudo tar -xzf /tmp/{} -C {}".format(fileName, completePath))
+        run("sudo tar -xzf /tmp/{} -C {}".format(last, foldName))
         # deletes archive from web server
-        run("sudo rm /tmp/{}".format(fileName))
+        run("sudo rm /tmp/{}".format(last))
         # moves the archive out of web static to be removed
-        run("sudo mv {}/web_static/* {}/".format(completePath, completePath))
+        run("sudo mv {}/web_static/* {}/".format(foldName, foldName))
         # removes the archive
-        run("sudo rm -rf {}/web_static".format(completePath))
+        run("sudo rm -rf {}/web_static".format(foldName))
         # deletes the symbolic link to the web server
         run("sudo rm -rf /data/web_static/current")
         # create a new sym link that links to new version of code
-        run("sudo ln -s {} /data/web_static/current".format(completePath))
+        run("sudo ln -s {} /data/web_static/current".format(foldName))
     except:
         return False
     return True
