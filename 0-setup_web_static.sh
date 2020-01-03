c0e79f6a42a3e01cc63823b7bd0824afc363dd7f
Michael <michael.sosa@holbertonschool.com>
2020-01-03 17:37:59 -0500

    Upadate follow symlinks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,16 +4,18 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-#sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx HTTP'
 # Create a folder
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
 sudo echo "<h1>Holberton School! :)</h1>" | sudo tee -a /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+# Checkers dont pass the next two lines so I had to do it in one
+#sudo chown -R ubuntu /data/
+#sudo chown -R :ubuntu /data/
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i --follow-symlink "25i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "17i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
