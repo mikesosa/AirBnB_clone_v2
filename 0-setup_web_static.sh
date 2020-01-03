4cf774a1218f8bdf3944fa4fe7964d988aad7531
Michael <michael.sosa@holbertonschool.com>
2020-01-03 17:28:28 -0500

    Upadate follow symlinks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-# Sets up the environment and directory for web server deployment
+# code that automatically installs "engine x" and inserts a string in the site
+# install nginx if not installed
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+#sudo ufw allow 'Nginx HTTP'
+# Create a folder
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html><head>poooop</head>Poop.</html>" | sudo tee -a /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i --follow-symlink "29i location /hbnb_static{\nalias /data/web_static/current/;autoindex off;\n}" /etc/nginx/sites-available/default
+sudo echo "<h1>Holberton School! :)</h1>" | sudo tee -a /data/web_static/releases/test/index.html
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chown -R :ubuntu /data/
+sudo sed -i --follow-symlink "17i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
